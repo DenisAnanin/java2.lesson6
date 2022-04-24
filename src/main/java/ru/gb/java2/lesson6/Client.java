5582a04551695e0079781199dbcdc2c0f4f2d8dd
DenisAnanin <denis-anapa@yandex.ru>
2022-04-24 23:39:07 +0300

    Домашнее задание Java2 Урок 6

--- src/main/java/ru/gb/java2/lesson6/Client.java
+++ src/main/java/ru/gb/java2/lesson6/Client.java
@@ -1,4 +1,70 @@
 package ru.gb.java2.lesson6;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.start();
+    }
+
+    private void start() {
+        try {
+            openConnection();
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                String msg = scanner.nextLine();
+                out.writeUTF(msg);
+                if ("/end".equalsIgnoreCase(msg)) {
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeConnection() throws IOException {
+        socket.close();
+        in.close();
+        out.close();
+    }
+
+    private void openConnection() throws IOException {
+        socket = new Socket("localhost", 50000);
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        String message = in.readUTF();
+                        System.out.println("Сервер " + message);
+                        if ("/end".equalsIgnoreCase(message)) {
+                            break;
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        closeConnection();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        thread.start();
+
+    }
 }
--- src/main/java/ru/gb/java2/lesson6/Server.java
+++ src/main/java/ru/gb/java2/lesson6/Server.java
@@ -1,4 +1,52 @@
 package ru.gb.java2.lesson6;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Server {
+
+    public static void main(String[] args) {
+
+        try (ServerSocket serverSocket = new ServerSocket(50000)) {
+            System.out.println("Сервер запущен...");
+            Socket socket = serverSocket.accept();
+            System.out.println("Соединение установлено");
+            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Scanner scanner = new Scanner(System.in);
+                    while (true) {
+                        String msg = scanner.nextLine();
+
+                        try {
+                            outputStream.writeUTF(msg);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+            thread.setDaemon(true);
+            thread.start();
+
+            while (true) {
+                String message = inputStream.readUTF();
+                System.out.println("Клиент: " + message);
+                if ("/end".equalsIgnoreCase(message)) {
+                    outputStream.writeUTF("/end");
+                    break;
+                }
+                outputStream.writeUTF("echo: " + message);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
